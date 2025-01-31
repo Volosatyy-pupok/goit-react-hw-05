ee19d822891a8e48e341203b913c24f7428b4ab2
Volosatyy-pupok <derevyanko.2001.oleg@gmail.com>
2025-01-31 19:13:24 +0200

    update

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import { Header } from "../../components/Navigation/Header";
+import { Navigation } from "../../components/Navigation/Navigation";
 export default function NotFound() {
   return (
     <>
-      <Header /> <h1>404 - Page Not Found</h1>
+      <Navigation /> <h1>404 - Page Not Found</h1>
       <p>We're sorry, but the page you are looking for could not be found.</p>
       <p>
         Please check the URL or go back to the <Link to="/">homepage</Link>.
